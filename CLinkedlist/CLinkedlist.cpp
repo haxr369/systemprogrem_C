0b92d0148775105f88727a5fbf5b89510b9c3f51
2018440087@office.uos.ac.kr <2018440087@office.uos.ac.kr>
2022-09-27 17:10:09 +0900

    포인터 변수로 노드를 표현

--- CLinkedlist/CLinkedlist.cpp
+++ CLinkedlist/CLinkedlist.cpp
@@ -1,5 +1,5 @@
 ﻿#include <stdio.h>
-
+#include <stdlib.h>
 //struct node {
 //	int data;
 //	struct node* pNext;
@@ -15,20 +15,22 @@ typedef struct node {
 } Node;
 
 
-
-
 int main()
 {
-	static int n; // 정적 변수: 프로그램이 시작 할 때 메모리를 할당된다.	
+	
 
 
 	//struct node a; // a라는 변수 이름의 node 자료구조의 메모리 확보.
-	Node a;
-	a.data = 10;  // a 노드에 데이터는 10
-	a.pNext = NULL; // a 노드에 포인터는 없음.
+	//Node a;
 
 	Node* pNode; //구조체 포인터 변수
-	pNode = &a;         //adress of a = pNode
+	static int n; // 정적 변수: 프로그램이 시작 할 때 메모리를 할당된다.	
+
+	pNode = (Node*) malloc(sizeof(Node)); //malloc(원하는 바이트 수) 
+										//=> 반환형이 void*(=니 마음대로 쓰라 but 형변환을 해라.) => 앞으로 pNode는  Node라는 구조를 사용하겠다.
+										// pNode의 값은 Node의 주소
+	pNode->data = 10000; // Node의 값을 10
+	pNode->pNext = NULL; // Node* 값이 NULL로
 	printf("member variable a is %d\n", pNode->data);
 
 
