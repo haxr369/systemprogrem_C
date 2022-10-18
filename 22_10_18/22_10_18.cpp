328b39bd7e7504ce75ca7cf0e1604811d16021e8
2018440087@office.uos.ac.kr <2018440087@office.uos.ac.kr>
2022-10-18 15:05:05 +0900

    7주차 코드입력

---
+++ 22_10_18/22_10_18.cpp
@@ -0,0 +1,55 @@
+﻿// 22_10_18.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#define WIN32_LEAN_AND_MEAN //??
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h> //windows API를 사용하려면 반드시 포함
+
+DWORD WINAPI ThreadFunc(LPVOID);// LPVOID ==> void*
+//대부분의 대문자 단어는 마이크로소프트사가 정의한 자료형이다.
+int main()
+{
+    HANDLE hThrd;
+    DWORD threadId;
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        hThrd = CreateThread(NULL,
+            0,
+            ThreadFunc,//함수 포인터
+            (LPVOID)i,//함수의 매개변수, 단 하나만 허락
+            0,
+            &threadId);
+        if (hThrd)
+        {
+            printf("Thread launched %d\n", i);
+            CloseHandle(hThrd);
+        }
+    }
+    // Wait for the threads to complete.
+    // We'll see a better way of doing this later.
+    Sleep(20000);//2000->20000
+
+    return EXIT_SUCCESS;
+}
+
+DWORD WINAPI ThreadFunc(LPVOID in)
+{
+    int i;
+    int n = (int)in;
+    for (i = 0; i < 10000; i++)//10->10000
+        printf("%d%d%d%d%d%d%d%d\n", n, n, n, n, n, n, n, n);
+    return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
