f829d04f29cceced16c96fc48419ee442c059857
2018440087@office.uos.ac.kr <2018440087@office.uos.ac.kr>
2022-11-08 16:20:10 +0900

    HW1, deadlock 구현

--- 22_11_08/22_11_08.cpp
+++ 22_11_08/22_11_08.cpp
@@ -1,20 +1,26 @@
-﻿// 22_11_08.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// 20211117samethread.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
+//2021.11.17 시스템프로그래밍 W12-2
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 
-#include <iostream>
+int main() {
+	CRITICAL_SECTION cs1;
+	InitializeCriticalSection(&cs1);//수행 후 깃발이 올라가 있음(signaled 들어올 수 있음)
+	EnterCriticalSection(&cs1);//수행 후 깃발을 내림 (nonsignaled 못 들어옴)
+	printf("we are taking cs1\n");
+	EnterCriticalSection(&cs1);//깃발 내렸는데 동일 쓰레드니까 통과 가능!
+	printf("we are taking cs1?????\n");
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	//Mutex
+	HANDLE hM;
+	hM = CreateMutex(NULL, FALSE, NULL); //signaled로 초기화
+	WaitForSingleObject(hM, INFINITE);   //signaled니까 넘어감. side-effect = non-signaled로 만든다.
+	printf("we are taking hM\n");
+	WaitForSingleObject(hM, INFINITE);   //동일 쓰레드에는 non-signaled된 Mutex를 통과가능!
+	printf("we are taking hM\n");
+
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
